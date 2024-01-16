6919fa98f918c849652f9e359485749874fca2eb
Thiago Souza <mistersouza@icloud.com>
2024-01-16 10:41:17 +0000

    Implement delete and edit comments functionalities

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,25 +1,79 @@
-import React from 'react'
-import styles from '../../styles/Comment.module.css'
+import React, { useState } from "react";
+import styles from "../../styles/Comment.module.css";
+import { useUser } from "../../Contexts/UserContext";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import { Media } from 'react-bootstrap'
-import Avatar from '../../components/Avatar'
+import { Media } from "react-bootstrap";
+import Avatar from "../../components/Avatar";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import { axiosResponse } from "../../api/axiosDefault";
+import CommentEditForm from "./CommentEditForm";
 
-function Comment({ profile_id, profile_image, owner, updated_at, content }) {
+function Comment({
+  profile_id,
+  profile_image,
+  owner,
+  updated_at,
+  content,
+  id,
+  setPost,
+  setComments,
+}) {
+  const [showEditForm, setShowEditForm] = useState(false);
+  const user = useUser();
+  const isOwner = user?.username === owner;
+
+  const handleDeleteClick = async () => {
+    try {
+      await axiosResponse.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-        <hr />
-        <Media className='d-flex'>
-            <Link to={`/profiles/${profile_id}`}>
-                <Avatar src={profile_image}/>
-            </Link>
-            <Media.Body>
-                <span className={styles.Owner}>{owner}</span>
-                <span className={styles.Date}>{updated_at}</span>
-                <p>{content}</p>
-            </Media.Body>
-        </Media>
+      <hr />
+      <Media className="d-flex">
+        <div className="d-flex flex-grow-1">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} />
+          </Link>
+          <Media.Body className="container">
+            <span className={styles.Owner}>{owner}</span>
+            <span className={styles.Date}>{updated_at}</span>
+            {showEditForm 
+                ? <CommentEditForm
+                    id={id}
+                    profileId={profile_id}
+                    content={content}
+                    profileImage={profile_image}
+                    setComments={setComments}
+                    setShowEditForm={setShowEditForm}
+                    /> 
+                : <p>{content}</p>
+            }
+          </Media.Body>
+        </div>
+        {isOwner && !showEditForm && (
+          <MoreDropdown
+            handleEditClick={() => setShowEditForm(true)}
+            handleDeleteClick={handleDeleteClick}
+          />
+        )}
+      </Media>
     </div>
-  )
+  );
 }
 
-export default Comment
+export default Comment;
---
+++ src/pages/comments/CommentEditForm.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import { axiosResponse } from "../../api/axiosDefault"
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+
+function CommentEditForm({ id, content, setShowEditForm, setComments }) {
+  const [formContent, setFormContent] = useState(content);
+
+  const handleInputChange = ({ target }) => {
+    setFormContent(target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosResponse.put(`/comments/${id}/`, {
+        content: formContent.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                content: formContent.trim(),
+                updated_at: "now",
+              }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Control
+          className={styles.Form}
+          as="textarea"
+          value={formContent}
+          onChange={handleInputChange}
+          rows={2}
+        />
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={styles.Button}
+          onClick={() => setShowEditForm(false)}
+          type="button"
+        >
+          cancel
+        </button>
+        <button
+          className={styles.Button}
+          disabled={!content.trim()}
+          type="submit"
+        >
+          save
+        </button>
+      </div>
+    </Form>
+  );
+}
+
+export default CommentEditForm;
