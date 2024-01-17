90eeec84363c6fa3eb7303a8dd2c11b5ab31a737
Thiago Souza <mistersouza@icloud.com>
2024-01-17 07:27:18 +0000

    Implement profile posts infinite scroll

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useUser } from "./Contexts/UserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
-
-
-
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 function App() {
   const user = useUser(); 
@@ -55,6 +53,7 @@ function App() {
               <Route exact path='/posts/create' render={() => <PostCreateForm />} />
               <Route exact path='/posts/:id' render={() => <PostPage />} />
               <Route exact path='/posts/:id/edit' render={() => <PostEditForm />} />
+              <Route exact path='/profiles/:id' render={() => <ProfilePage />} />
               <Route render={() => <p>Page not found</p>} />
             </Switch>
           </Container>
---
+++ src/pages/profiles/ProfilePage.js
@@ -0,0 +1,149 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+
+import styles from "../../styles/ProfilePage.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+import Asset from "../../components/Asset";
+import PopularProfiles from "./PopularProfiles";
+import Post from "../posts/Post";
+import NoResults from '../../assets/no-results.png'
+import { useUser } from "../../Contexts/UserContext";
+import { useParams } from 'react-router'
+import { axiosRequest } from "../../api/axiosDefault";
+import { useProfile, useSetProfile } from "../../Contexts/ProfileContext";
+import { Button, Image } from "react-bootstrap";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+
+function ProfilePage() {
+  const [profilePosts, setProfilePosts] = useState({ results: [] });
+  const [ isLoaded, setIsLoaded ] = useState(false);
+  const user = useUser();
+  const setProfile = useSetProfile()
+  const { id } = useParams()
+  const { pageProfiles } = useProfile(); 
+  const [ profile ] = pageProfiles.results;
+  const isOwner = user?.username === profile?.owner; 
+    
+  useEffect(() => {
+    (async () => {
+        try {
+            const [{ data: pageProfile }, { data: profilePosts }] = await Promise.all([
+                axiosRequest.get(`/profiles/${id}/`),
+                axiosRequest.get(`/posts/?owner__profile=${id}`),
+            ])
+            console.log({pageProfile})
+            setProfile(prevProfile => ({
+                ...prevProfile,
+                pageProfiles: { results: [pageProfile] }
+            }))
+            setProfilePosts(profilePosts);
+            setIsLoaded(true);
+        } catch (error) {
+            console.log(error)
+        }
+    })()
+  }, [setProfile, id])
+
+  const mainProfile = (
+    <>
+      <Row noGutters className="px-3 text-center">
+        <Col lg={3} className="text-lg-left">
+          <Image 
+            className={styles.ProfileImage}
+            roundedCircle
+            src={profile?.image}
+          />
+        </Col>
+        <Col lg={6}>
+          <h3 className="m-2">{profile?.owner}</h3>
+          <Row className='justify-content-center no-gutters'>
+            <Col xs={3} className="my-2">
+                <div>{profile?.posts_count}</div>
+                <div>posts</div>
+            </Col>
+            <Col xs={3} className="my-2">
+                <div>{profile?.followers_count}</div>
+                <div>followers</div>
+            </Col>
+            <Col xs={3} className="my-2">
+                <div>{profile?.following_count}</div>
+                <div>following</div>
+            </Col>
+          </Row>
+          
+        </Col>
+        <Col lg={3} className="d-flex justify-content-center">
+        {user && !isOwner && (profile?.following_id ? (
+            <Button
+                className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+                onClick={() => {}}
+            >unfolllow</Button>
+        ) : (
+            <Button
+                className={`${btnStyles.Button} ${btnStyles.Black}`}
+                onClick={() => {}}
+            >follow</Button>
+        ))}
+        </Col>
+        {profile?.content && <Col className="p-3">{profile.content}</Col>}
+      </Row>
+    </>
+  );
+
+  const mainProfilePosts = (
+    <>
+      <hr />
+      <p className="text-center">{profile?.owner}'s posts</p>
+      <hr />
+      {profilePosts.results.length ? (
+        <InfiniteScroll 
+          children={
+            profilePosts.results.map(post => (
+              <Post key={post.id} {...post} setProfilePosts={setProfilePosts} />
+            ))}
+          dataLength={profilePosts.results.length}
+          loader={<Asset spinner />}
+          hasMore={!!profilePosts.next}
+          next={() => fetchMoreData(profilePosts, setProfilePosts)}
+        />
+      ) : (
+        <Container>
+          <Asset 
+            src={NoResults} 
+            message={`No results found, ${profile?.owner} hasn't posted yet`}
+          />
+        </Container>
+      )}
+    </>
+  );
+
+  return (
+    <Row>
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <PopularProfiles mobile />
+        <Container className={appStyles.Content}>
+          {isLoaded ? (
+            <>
+              {mainProfile}
+              {mainProfilePosts}
+            </>
+          ) : (
+            <Asset spinner />
+          )}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
+  );
+}
+
+export default ProfilePage;
